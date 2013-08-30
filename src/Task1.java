a17c7bab10b2010a00dc9deed2c620dfcd83dbef
Alexander Whillas <whillas@gmail.com>
2013-08-30 17:33:12 +1000

    added even distribution (i.e. already sorted)

--- src/Task1.java
+++ src/Task1.java
@@ -5,11 +5,14 @@ import java.io.InputStreamReader;
 class Task1 {
 	public static void main(String[] args) {
 		String input = "";
-		assert args.length == 2;
+		assert args.length >= 2;
 		try{
-			int length = Integer.parseInt(args[0]);
-			int num = Integer.parseInt(args[1]);
-			// is an integer!
+			int length = Integer.parseInt(args[0]);	// length of each line
+			int num = Integer.parseInt(args[1]);	// number of lines
+			String type = "";
+			if(args.length >= 2)
+				type = args[2];
+/*
 			try{
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				String in;
@@ -21,23 +24,47 @@ class Task1 {
 					.substring(0, Math.min(input.length(), 1000))
 				;
 				if(input.isEmpty()) {
+*/
 					input = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//}
+				if (type.equals("e")) {
+					Task1.even(length, num, input);
+				} else {
+					Task1.random(length, num, input);
 				}
-//				System.out.println("Input: "+input);
-				for(int i = 0; i < num; i++) {
-					String output = "";
-					for(int j = 0; j < length; j++) {
-						int x = (int) (Math.random() * input.length());
-						output += input.charAt(x);
-					}
-					System.out.println(output);
-				}
+/*				
 			} catch(IOException io) {
 				io.printStackTrace();
 			}
+*/
 		} catch (NumberFormatException e) {
 			// not an integer!
 			System.out.println("We want 2 integers stupid");
+		}
+	}
+	
+	// Even amount of each character
+	private static void even(int length, int num, String input) {
+		int x = 0;
+		for(int i = 0; i < num; i++) {
+			String output = "";
+			for(int j = 0; j < length; j++) {
+				output += input.charAt(x);
+			}
+			if(x < input.length() - 1)
+				x++;
+			System.out.println(output);
+		}
+	}
+	
+	private static void random(int length, int num, String input) {
+		for(int i = 0; i < num; i++) {
+			String output = "";
+			for(int j = 0; j < length; j++) {
+				int x = (int) (Math.random() * input.length());
+				output += input.charAt(x);
+			}
+			System.out.println(output);
 		}
 	}
 }
