0236655a461050c870a0610be3b4f742acf42b63
Alexander Whillas <whillas@gmail.com>
2013-09-16 14:59:02 +1000

    MyHeap interface

---
+++ src/unsw/comp9024/MyHeap.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package unsw.comp9024;
+
+/**
+ * @author Alexander Whillas (z3446737) <whillas@gmail.com>
+ */
+public interface MyHeap {
+
+	public void insert (String key, int heapWeight);
+	
+	public boolean contains (String s);
+}
