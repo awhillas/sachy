ee6ac338b07083531c37aeaa1fc29327f31f618e
Alexander Whillas <whillas@gmail.com>
2013-09-26 13:15:45 +1000

    Initial setup of the project

---
+++ src/unsw/comp9024/Chess/DeepTeal.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package unsw.comp9024.Chess;
+
+/**
+ * @author Alexander Whillas <z3446737@student.unsw.edu.au>
+ *
+ */
+public class DeepTeal implements ChessThinker{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void fromString(String boardString) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean blackIsInCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean blackIsInCheckMate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean whiteCanMateInOneMove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void makeWhiteMateMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
