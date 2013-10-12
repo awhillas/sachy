f980ccf2549a7710c6011ef4158b16257b17637a
Alexander Whillas <whillas@gmail.com>
2013-10-12 17:13:23 +1100

    Finished basic Graph class and tests. Graph Interface is also finished. Removed Vertex class as not needed.

--- src/unsw/comp9024/Graph/AdjacencyMatrix.java
+++ src/unsw/comp9024/Graph/AdjacencyMatrix.java
@@ -3,6 +3,9 @@
  */
 package unsw.comp9024.Graph;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Adjacency Matrix for an undirected graph i.e. matrix is symmetrical.
  * @author arbw870
@@ -43,6 +46,53 @@ public class AdjacencyMatrix implements Graph {
 		}
 		return count;
 	}
+
+	private int indexOf(String v) {
+		for (int i = 0; i < vertices.length; i++) {
+			if (vertices[i].compareTo(v) == 0) {
+				return i;
+			}
+		}
+		return -1;	// error
+	}	
+	
+	@Override
+	public int degree(String v) {
+		int index = this.indexOf(v);
+		int count = 0;
+		if (index != -1) {	// maybe should throw an error here?
+			for (int i = 0; i < edges[index].length; i++) {
+				if(edges[index][i] != 0) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	@Override
+	public String[] adjacentVertices(String v) {
+		ArrayList<String> out = new ArrayList<String>();
+		int index = this.indexOf(v);
+		if (index != -1) {	// maybe should throw an error here?
+			for (int i = 0; i < edges[index].length; i++) {
+				if(edges[index][i] != 0) {
+					out.add(vertices[i]);
+				}
+			}
+		}		
+		return out.toArray(new String[0]);
+	}
+
+	@Override
+	public boolean areAdjacent(String v, String w) {
+		// Input checking needed here.
+		return edges[indexOf(v)][indexOf(w)] != 0;
+	}
+	
+	public int getEdgeCost(String v, String w) {
+		return edges[indexOf(v)][indexOf(w)];
+	}
 		
 	/**
 	 * @param args
@@ -63,6 +113,52 @@ public class AdjacencyMatrix implements Graph {
 	    
 	    assert graph.numEdges() == 7: "Grpah should have 7 edges found "+graph.numEdges();
 	    assert graph.numVertices() == 6: "Graph should have 6 vertices, found "+graph.numVertices();
+	    
+	    assert graph.areAdjacent("A", "B");
+	    assert graph.areAdjacent("A", "C");
+	    assert graph.areAdjacent("A", "D");
+	    assert graph.areAdjacent("C", "B");
+	    assert graph.areAdjacent("D", "E");
+	    assert graph.areAdjacent("D", "E");
+	    
+	    assert graph.degree("A") == 3;
+	    assert graph.degree("B") == 3;
+	    assert graph.degree("C") == 2;
+	    assert graph.degree("D") == 3;
+	    assert graph.degree("E") == 2;
+	    assert graph.degree("F") == 1;
+	    
+	    String[] adj = graph.adjacentVertices("A");
+	    assert adj.length == 3;
+	    assert Arrays.asList(adj).contains("B");
+	    assert Arrays.asList(adj).contains("C");
+	    assert Arrays.asList(adj).contains("D");
+	    
+	    adj = graph.adjacentVertices("B");
+	    assert adj.length == 3;
+	    assert Arrays.asList(adj).contains("A");
+	    assert Arrays.asList(adj).contains("C");
+	    assert Arrays.asList(adj).contains("E");
+	    
+	    adj = graph.adjacentVertices("C");
+	    assert adj.length == 2;
+	    assert Arrays.asList(adj).contains("A");
+	    assert Arrays.asList(adj).contains("B");
+
+	    adj = graph.adjacentVertices("D");
+	    assert adj.length == 3;
+	    assert Arrays.asList(adj).contains("A");
+	    assert Arrays.asList(adj).contains("E");
+	    assert Arrays.asList(adj).contains("F");
+	    
+	    adj = graph.adjacentVertices("E");
+	    assert adj.length == 2;
+	    assert Arrays.asList(adj).contains("B");
+	    assert Arrays.asList(adj).contains("D");
+	    
+	    adj = graph.adjacentVertices("F");
+	    assert adj.length == 1;
+	    assert Arrays.asList(adj).contains("D");	    
 	    
 	    System.out.println("PASSED all tests!");
 	}
